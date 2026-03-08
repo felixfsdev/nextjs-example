754f35a4eeb81972f5b0c0b542d5f14489a3ed32
FaseehFS <faseeh1080@gmail.com>
2026-03-08 17:37:09 +0530

    Use Image

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <p>Hello World!</p>
+    <div className="mx-auto w-full max-w-4xl p-5">
+      <div className="w-full max-w-xs p-6 mx-auto">
+        <Image
+          src="/images/out-of-office.png"
+          alt="Image"
+          width={500}
+          height={500}
+        />
+      </div>
+      <h1 className="text-4xl text-center">Welcome!</h1>
+      <p className="text-center mt-4 mb-10">Thanks for visiting my website</p>
+      <hr className="text-gray-300" />
     </div>
   );
 }

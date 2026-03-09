1b08878251f83c0f3d85cb7f024aa6178103bd6b
FaseehFS <faseeh1080@gmail.com>
2026-03-09 16:11:07 +0530

    Add more content

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,7 @@ export default function Home() {
       <p className="text-center mt-4 mb-10">Thanks for visiting my website</p>
 
       <hr className="text-gray-300" />
+
       <div className="flex flex-col md:flex-row w-full p-6">
         <div className="flex-1 flex justify-center items-center p-2">
           <Image
@@ -29,6 +30,47 @@ export default function Home() {
         <div className="flex-1 p-4 flex flex-col justify-center">
           <h2 className="text-3xl text-center pb-4">What is this?</h2>
           <p className="text-center">This is a Next.js application.</p>
+        </div>
+      </div>
+
+      <hr className="text-gray-300" />
+
+      <div className="flex flex-col-reverse md:flex-row w-full p-6">
+        <div className="flex-1 p-4 flex flex-col justify-center">
+          <h2 className="text-3xl text-center pb-4">Why I made this</h2>
+          <p className="text-center">To learn Next.js</p>
+        </div>
+        <div className="flex-1 flex justify-center items-center p-2">
+          <Image
+            src="/images/working-at-home.png"
+            alt="Image"
+            width={500}
+            height={500}
+            className="object-contain w-full max-w-xs"
+          />
+        </div>
+      </div>
+
+      <hr className="text-gray-300" />
+
+      <div className="flex flex-col md:flex-row w-full p-6">
+        <div className="flex-1 flex justify-center items-center p-2">
+          <Image
+            src="/images/un-draw-1000.png"
+            alt="Image"
+            width={500}
+            height={500}
+            className="object-contain w-full max-w-sm"
+          />
+        </div>
+
+        <div className="flex-1 p-4 flex flex-col justify-center">
+          <h2 className="text-3xl text-center pb-4">
+            From where did you get these images?
+          </h2>
+          <p className="text-center">
+            From <a href="https://undraw.co/">Undraw.co</a>
+          </p>
         </div>
       </div>
     </div>

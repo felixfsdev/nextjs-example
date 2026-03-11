7a08aa8877576088672519c51516776434c603f6
FaseehFS <faseeh1080@gmail.com>
2026-03-11 16:42:53 +0530

    Enhance LandingPageNavbar styling with additional border and padding

--- components/LandingPageNavbar.tsx
+++ components/LandingPageNavbar.tsx
@@ -7,7 +7,7 @@ export default function LandingPageNavbar() {
   const [open, setOpen] = useState(false);
 
   return (
-    <nav className="sticky top-0 w-full bg-white p-2">
+    <nav className="sticky top-0 w-full bg-white p-2 border-b border-gray-300">
       <div className="flex items-center justify-between">
         <h1 className="text-2xl">Next.js Example</h1>
 
@@ -23,11 +23,11 @@ export default function LandingPageNavbar() {
       </div>
 
       <div
-        className={`overflow-hidden transition-all duration-300 md:hidden ${
+        className={`absolute left-0 right-0 bg-white overflow-hidden transition-all duration-300 md:hidden border-b border-gray-300 ${
           open ? "max-h-40 opacity-100 pt-4" : "max-h-0 opacity-0"
         }`}
       >
-        <ul className="flex flex-col gap-4">
+        <ul className="flex flex-col gap-4 p-2">
           <ExternalLink href="/">Home</ExternalLink>
           <ExternalLink href="">Stuff</ExternalLink>
           <ExternalLink href="">More stuff</ExternalLink>

8b61a8f30b9f003036fff733349f2d0b7e5637bd
FaseehFS <faseeh1080@gmail.com>
2026-03-13 21:53:27 +0530

    Update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const links = (
   <>
@@ -14,9 +15,29 @@ const links = (
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
+  const [atTop, setAtTop] = useState(true);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setAtTop(window.scrollY === 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    handleScroll();
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <nav className="sticky top-0 w-full p-2 border-b bg-bg border-border">
+    <nav
+      className={`fixed top-0 w-full transition-all duration-100 border-b ${
+        atTop && !open && pathname === "/"
+          ? "bg-transparent border-transparent p-4"
+          : "bg-bg/70 backdrop-blur-md border-border p-2"
+      }`}
+    >
       <div className="flex items-center justify-between">
         <Link href="/" className="text-2xl">
           Next.js Example
@@ -30,8 +51,8 @@ export default function Navbar() {
       </div>
 
       <div
-        className={`absolute left-0 right-0 bg-bg overflow-hidden mt-2 transition-all duration-300 md:hidden border-b border-border ${
-          open ? "max-h-60 pt-4" : "max-h-0"
+        className={`overflow-hidden transition-all duration-300 md:hidden  ${
+          open ? "max-h-60 mt-4" : "max-h-0"
         }`}
       >
         <ul className="flex flex-col gap-4 p-2">{links}</ul>

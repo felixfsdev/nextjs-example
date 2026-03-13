22cc3afdf297f116912a860ff624bd9a65dfd395
FaseehFS <faseeh1080@gmail.com>
2026-03-13 10:55:28 +0530

    Apply light theme by default

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="light">
       <body
         className={`bg-bg text-text ${geistSans.variable} ${geistMono.variable} antialiased`}
       >

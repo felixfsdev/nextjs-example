54d18f1e864e97f0eda40c618b935c482e3f9ce0
FaseehFS <faseeh1080@gmail.com>
2026-03-20 10:22:50 +0530

    Add sign in with resend

--- app/(with-nav)/sign-in/page.tsx
+++ app/(with-nav)/sign-in/page.tsx
@@ -4,9 +4,22 @@ import GithubIcon from "@/components/GitHubIcon";
 import { signIn } from "next-auth/react";
 
 export default function SignIn() {
+  const resendAction = (formData: FormData) => {
+    signIn("resend", Object.fromEntries(formData));
+  };
+
   return (
     <div className="w-full max-w-lg mx-auto p-4">
       <h1 className="text-2xl mb-4">Choose a sign in method</h1>
+
+      <form action={resendAction}>
+        <label htmlFor="email-resend">
+          Email
+          <input type="email" id="email-resend" name="email" />
+        </label>
+        <input type="submit" value="Signin with Resend" />
+      </form>
+
       <button
         className="flex items-center justify-center w-full p-1 gap-2 hover:bg-bg-hover hover:cursor-pointer border border-border rounded"
         onClick={() => signIn("github")}

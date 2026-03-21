7bd420aede3004cdffe18691cf8174f2da853488
FaseehFS <faseeh1080@gmail.com>
2026-03-21 21:26:19 +0530

    Add fs image

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Button from "@/components/ui/Button";
+import Image from "next/image";
 import Link from "next/link";
+import FSImage from "@/assets/fs-nobg-cropped.png";
 
 export default function LandingPage() {
   return (
     <div className="flex flex-col gap-4 w-full min-h-screen justify-center items-center max-w-lg mx-auto p-4">
-      <h1 className="text-4xl">Welcome</h1>
+      <Image src={FSImage} className="w-40" alt="fs Image" />
+      <h1 className="text-6xl">Welcome</h1>
       <Link href="/sign-in/">
         <Button>Sign in</Button>
       </Link>

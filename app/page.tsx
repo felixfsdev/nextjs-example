dbd07db1921def29383b46604ce7c9a6a97d741e
FaseehFS <faseeh1080@gmail.com>
2026-03-22 18:17:29 +0530

    Add priority attribute to image

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import FSImage from "@/assets/fs-nobg-cropped.png";
 export default function LandingPage() {
   return (
     <div className="flex flex-col gap-4 w-full min-h-[75vh] justify-center items-center max-w-lg mx-auto p-4">
-      <Image src={FSImage} className="w-40" alt="fs Image" />
+      <Image src={FSImage} className="w-40" alt="fs Image" priority />
       <h1 className="text-6xl">Welcome</h1>
       <Link href="/sign-in/">
         <Button>Sign in</Button>

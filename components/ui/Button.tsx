1e53371babd28fc90f162ba296760ae95eedb353
FaseehFS <faseeh1080@gmail.com>
2026-03-22 21:39:44 +0530

    Remove unused styles

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -3,24 +3,16 @@ import clsx from "clsx";
 
 type ButtonProps = PropsWithChildren<{
   className?: string;
-  muted?: boolean;
 }> &
   ButtonHTMLAttributes<HTMLButtonElement>;
 
-export default function Button({
-  className,
-  muted = false,
-  children,
-  ...props
-}: ButtonProps) {
+export default function Button({ className, children, ...props }: ButtonProps) {
   return (
     <button
       className={clsx(
         "rounded py-1 px-4 cursor-pointer transition-all",
+        "bg-primary hover:bg-primary-hover active:bg-primary-active text-on-primary",
         "disabled:bg-primary-disabled disabled:cursor-default",
-        muted
-          ? "bg-surface hover:bg-surface/90 active:bg-surface-90"
-          : "bg-primary hover:bg-primary-hover active:bg-primary-active text-on-primary",
         className,
       )}
       {...props}

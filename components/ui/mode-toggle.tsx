cdcf340401541c9f61293a0edd6aedf8f1e9e8b8
FaseehFS <faseeh1080@gmail.com>
2026-03-25 21:11:32 +0530

    style: add animations for mode toggle

--- components/ui/mode-toggle.tsx
+++ components/ui/mode-toggle.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import * as React from "react";
-import { Moon, Sun } from "lucide-react";
+import { Moon, Sun, Contrast } from "lucide-react";
 import { useTheme } from "next-themes";
 import { cn } from "@/lib/utils";
+import { useState, useEffect } from "react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -15,13 +16,40 @@ import {
 
 export function ModeToggle() {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+        <Button variant="outline" size="icon" className="relative">
+          <Sun
+            className={cn(
+              "h-5 w-5 transition-all duration-300",
+              mounted && theme === "light"
+                ? "scale-100 rotate-0"
+                : "scale-0 rotate-90",
+            )}
+          />
+          <Moon
+            className={cn(
+              "absolute h-5 w-5 transition-all duration-300",
+              mounted && theme === "dark"
+                ? "scale-100 rotate-0"
+                : "scale-0 -rotate-90",
+            )}
+          />
+          <Contrast
+            className={cn(
+              "absolute h-5 w-5 transition-all duration-300",
+              mounted && theme === "system"
+                ? "scale-100 rotate-0"
+                : "scale-0 rotate-90",
+            )}
+          />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>

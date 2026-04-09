c857f60162a65e51cd9d610966ed026b3e418620
FaseehFS <faseeh1080@gmail.com>
2026-04-09 11:59:24 +0530

    add loading page for (user)

---
+++ app/(user)/loading.tsx
@@ -0,0 +1,9 @@
+import LoadingDots from "@/components/ui/loading-dots";
+
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center min-h-[75vh] w-full">
+      <LoadingDots />
+    </div>
+  );
+}
